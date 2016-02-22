717e34f5cfe684edfd59ef403c12677cb959cff3
BethLi <i7674924@bournemouth.ac.uk>
2016-02-22 12:04:00 +0000

    added README.md

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "SDLOpenGL.h"
+#include <ngl/NGLInit.h>
+
+void initalizeGL();
+void renderGL();
+
+int main(int argc, char *argv[])
+{
+    SDLOpenGL window("test",0,0,1024,720);
+    window.makeCurrent();
+    initalizeGL();
+    while(true)
+    {
+        renderGL();
+        window.swapWindow();
+        SDL_Delay(10);
+    }
+    std::cout<<"working\n";
+
+}
+
+void initalizeGL()
+{
+    ngl::NGLInit::instance();
+    glClearColor(0.8,0.8,0.8,1.0);
+
+}
+
+void renderGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
